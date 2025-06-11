580016dca47e0caa0932b395447502a06868be11
wei2much <iam@wei2mu.ch>
2025-06-11 13:55:49 +1000

    nuke all code, start over

---
+++ .astro/content-modules.mjs
@@ -0,0 +1,4 @@
+
+export default new Map([
+["src/content/blog/markdown-features.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fmarkdown-features.mdx&astroContentModuleFlag=true")]]);
+		
